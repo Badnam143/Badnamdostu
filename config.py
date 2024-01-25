f2630f48e782482a0dfc4968ede1ca1a564089cc
Badnam143 <154507591+Badnam143@users.noreply.github.com>
2024-01-26 04:40:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/SubhiChiku/DOSTU",
+    "https://github.com/Badnam143/DOSTU",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Dostu")
 GIT_TOKEN = getenv(
