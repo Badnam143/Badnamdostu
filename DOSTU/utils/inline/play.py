2ca3e0eb04b66b3dbd08985094b605033ed2674a
Badnam143 <154507591+Badnam143@users.noreply.github.com>
2024-01-26 04:58:39 +0530

    Update play.py

--- DOSTU/utils/inline/play.py
+++ DOSTU/utils/inline/play.py
@@ -33,25 +33,31 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "✦ᴅᴏꜱᴛᴜ✦—————————"
+        bar = "✦———————————————"
     elif 10 < umm < 20:
-        bar = "—✦ᴅᴏꜱᴛᴜ✦————————"
+        bar = "—✦——————————————"
     elif 20 <= umm < 30:
-        bar = "——✦ᴅᴏꜱᴛᴜ✦———————"
+        bar = "——✦—————————————"
     elif 30 <= umm < 40:
-        bar = "———✦ᴅᴏꜱᴛᴜ✦——————"
+        bar = "———✦————————————"
     elif 40 <= umm < 50:
-        bar = "————✦ᴅᴏꜱᴛᴜ✦—————"
+        bar = "————✦———————————"
     elif 50 <= umm < 60:
-        bar = "—————✦ᴅᴏꜱᴛᴜ✦————"
+        bar = "—————✦——————————"
     elif 60 <= umm < 70:
-        bar = "——————✦ᴅᴏꜱᴛᴜ✦———"
+        bar = "——————✦—————————"
     elif 70 <= umm < 80:
-        bar = "———————✦ᴅᴏꜱᴛᴜ✦——"
+        bar = "———————✦————————"
     elif 80 <= umm < 95:
-        bar = "————————✦ᴅᴏꜱᴛᴜ✦—"
+        bar = "———————————✦————"
     else:
-        bar = "—————————✦ᴅᴏꜱᴛᴜ✦"
+        bar = "—————————————✦——"
+        [
+            InlineKeyboardButton(
+                text=f"{played} {bar} {dur}",
+                callback_data="GetTimer",
+            )
+        ],
     buttons = [
         [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
@@ -60,12 +66,8 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
-        [
-            InlineKeyboardButton(
-                text=f"{played} {bar} {dur}",
-                callback_data="GetTimer",
-            )
-        ],
+
+        
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
